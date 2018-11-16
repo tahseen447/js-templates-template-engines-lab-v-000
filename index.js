07f7dc8eca8394b911b8f177d4906df165fffdb1
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-16 23:44:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ function createPost() {
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+}
